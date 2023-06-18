67b1839d1b1aebdfe911169ae8cdf9a605a1b536
MarynaShchedrova <shchedrovamaryna@gmail.com>
2023-06-18 22:50:00 +0300

    commit

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+import css from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'redux/filterSlice';
+import { getFilter } from 'redux/selectors';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
+  return (
+    <label className={css.filterLabel}>
+      <input
+        className={css.filterName}
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={e => dispatch(setFilter(e.target.value))}
+        placeholder="Find contacts by name"
+      />
+    </label>
+  );
+};
